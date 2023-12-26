678941bb54e88e771fd5691c4498d02be2beb051
Evandro Calado <evandrocalado07@gmail.com>
2023-12-26 20:22:32 -0300

    ✨ feat(pages): sign in

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin() {
+    if (email === "" || password === "") {
+      return;
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../../assets/logo.png")}
+        style={styles.logo}
+      />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Digite seu email"
+          placeholderTextColor={"#f0f0f0"}
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          placeholder="Digite sua senha"
+          placeholderTextColor={"#f0f0f0"}
+          style={styles.input}
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleLogin}
+        >
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#1d1d2e",
+  },
+
+  logo: {
+    marginBottom: 18,
+  },
+
+  inputContainer: {
+    width: "95%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 32,
+    paddingHorizontal: 14,
+  },
+
+  input: {
+    width: "95%",
+    height: 40,
+    backgroundColor: "#101026",
+    marginBottom: 12,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    color: "#FFF",
+  },
+
+  button: {
+    width: "95%",
+    height: 40,
+    backgroundColor: "#3fffa3",
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#101026",
+  },
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,28 @@
+import { ActivityIndicator, View } from "react-native";
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
 
 export default function Routes() {
   const isAuthenticated = false;
   const loading = false;
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#1d1d2e",
+        }}
+      >
+        <ActivityIndicator
+          size={60}
+          color={"#fff"}
+        />
+      </View>
+    );
+  }
 
   return isAuthenticated ? <AppRoutes /> : <AuthRoutes />;
 }
